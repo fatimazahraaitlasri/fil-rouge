302841931ed6b0e4914718d37548d86f375a2868
Said Oudouane <saidoudouane@gmail.com>
2022-06-24 07:05:53 +0100

    add comments table migration

---
+++ app/database/comment.sql
@@ -0,0 +1,10 @@
+create table if not exists comments
+(
+    id          int auto_increment primary key,
+    user_id     int  not null,
+    property_id int  not null,
+    content     text not null,
+    created_at  datetime default now(),
+    foreign key (user_id) references users (id),
+    foreign key (property_id) references properties (id)
+);
