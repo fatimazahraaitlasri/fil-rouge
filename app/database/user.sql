e3c1d6c1a37c7cfc70f5d99f705f8b01f9541f71
Said Oudouane <saidoudouane@gmail.com>
2022-06-24 07:02:51 +0100

    add users table migration

--- app/database/user.sql
+++ app/database/user.sql
@@ -1,1 +1,11 @@
-create
+create table if not exists users
+(
+    id         int auto_increment primary key,
+    name       varchar(255) not null,
+    email      varchar(255) not null unique,
+    password   varchar(255) not null,
+    created_at datetime                      default now(),
+    about      text,
+    role       enum ('guest','host','admin') default 'guest',
+    avatar     varchar(255)
+)
