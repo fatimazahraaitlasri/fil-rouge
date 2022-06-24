e7d76a21aa20bc10de3017a116aaac453f63b9d3
fatimazahra ait lasri <fatimazahraaitlasri96@gmail.com>
2022-06-24 14:32:23 +0100

    remove vuejs

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,46 +5,44 @@ mix.disableSuccessNotifications();
 mix.setPublicPath("public");
 
 function findFiles(dir) {
-  const fs = require("fs");
-  return fs.readdirSync(dir).filter((file) => {
-    return fs.statSync(`${dir}/${file}`).isFile();
-  });
+    const fs = require("fs");
+    return fs.readdirSync(dir).filter((file) => {
+        return fs.statSync(`${dir}/${file}`).isFile();
+    });
 }
 
 mix.webpackConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve("app/resources/sass"),
+    resolve: {
+        alias: {
+            "@": path.resolve("app/resources/sass"),
+        },
     },
-  },
 });
 
 function buildSass(dir, dest) {
-  const source = `app/${dir}`;
-  findFiles(source).forEach(function (file) {
-    if (!file.startsWith("_")) {
-      mix.sass(`${source}/${file}`, dest);
-    }
-  });
+    const source = `app/${dir}`;
+    findFiles(source).forEach(function (file) {
+        if (!file.startsWith("_")) {
+            mix.sass(`${source}/${file}`, dest);
+        }
+    });
 }
 
 function buildJS(dir, dest) {
-  const source = `app/${dir}`;
-  findFiles(source).forEach(function (file) {
-    mix.js(`${source}/${file}`, dest);
-  });
+    const source = `app/${dir}`;
+    findFiles(source).forEach(function (file) {
+        mix.js(`${source}/${file}`, dest);
+    });
 }
 
-buildJS("resources/js", "js");
 buildSass("resources/sass", "css");
 
 mix.options({
-  processCssUrls: false,
+    processCssUrls: false,
 });
-mix.vue();
 
 mix.browserSync({
-  proxy: "http://localhost/fil-rouge-youcode",
-  notify: false,
-  open: false, // todo enable open for dev later
+    proxy: "http://localhost/fil-rouge-youcode",
+    notify: false,
+    open: false, // todo enable open for dev later
 });
